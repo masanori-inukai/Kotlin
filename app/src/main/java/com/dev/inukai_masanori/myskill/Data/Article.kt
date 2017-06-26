fd5817c1b88972831ab887d94282d6ff3ede0804
masanori-inukai <guitarist@hotmail.co.jp>
2017-06-26 14:43:21 +0900

    data クラス作成

---
+++ app/src/main/java/com/dev/inukai_masanori/myskill/Data/Article.kt
@@ -0,0 +1,7 @@
+package com.dev.inukai_masanori.myskill.Data
+
+/**
+ * Created by inukai_masanori on 2017/06/26.
+ */
+
+data class Article (val id: String, val title: String, val url: String, val user: User)
---
+++ app/src/main/java/com/dev/inukai_masanori/myskill/Data/User.kt
@@ -0,0 +1,7 @@
+package com.dev.inukai_masanori.myskill.Data
+
+/**
+ * Created by inukai_masanori on 2017/06/26.
+ */
+
+data class User (val id: String, val name: String, val profileImageUrl: String)
