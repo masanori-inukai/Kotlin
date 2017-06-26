87b2bcc3b4cf112876a1e1499ac7096588f58bf5
masanori-inukai <guitarist@hotmail.co.jp>
2017-06-26 15:20:08 +0900

    とりあえず表示されるように

--- app/src/main/java/com/dev/inukai_masanori/myskill/MainActivity.kt
+++ app/src/main/java/com/dev/inukai_masanori/myskill/MainActivity.kt
@@ -2,11 +2,25 @@ package com.dev.inukai_masanori.myskill
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.dev.inukai_masanori.myskill.Data.Article
+import com.dev.inukai_masanori.myskill.Data.User
+import com.dev.inukai_masanori.myskill.UI.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val articleView = ArticleView(applicationContext)
+        articleView.setArticle(
+                Article(
+                        id = "111",
+                        title = "kotlin",
+                        url = "http:/test.com",
+                        user = User(id = "222", name = "inukai", profileImageUrl = "")
+                )
+        )
+
+        setContentView(articleView)
     }
 }
